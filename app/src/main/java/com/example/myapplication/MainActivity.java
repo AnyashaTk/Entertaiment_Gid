971e2de29314d2af0e185448b17ea103c9640b89
NastyaGordienko <wind05@list.ru>
2022-01-09 14:50:45 +0300

    Update MainActivity.java

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,5 +1,3 @@
-package com.example.myapplication;
-
 package com.example.mapapp;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -26,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
         Button btnMap3 = (Button) findViewById(R.id.btnMap3);
         Button btnMap4 = (Button) findViewById(R.id.btnMap4);
         Button btnMap5 = (Button) findViewById(R.id.btnMap5);
-
+/*
         View.OnClickListener oclbtnMap = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -66,5 +64,99 @@ public class MainActivity extends AppCompatActivity {
         btnMap3.setOnClickListener(oclbtnMap);
         btnMap4.setOnClickListener(oclbtnMap);
         btnMap5.setOnClickListener(oclbtnMap);
+*/
+        View.OnClickListener oclbtnMap2 = new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent2 = new Intent();
+
+                intent2.setAction(Intent.ACTION_VIEW);
+
+                intent2.setData(Uri.parse("http://www.vk.com"));
+
+                // Uri gmmIntentUri = Uri.parse("гео:37.7749,-122.4192?q=" + Uri.encode("1-й и Пайк, Сиэтл"));
+                // Uri gmmIntentUri = Uri.parse("гео:37.7749,-122.4194");
+
+                intent2.setData(Uri.parse("geo:48.206487, 16.363460"));
+
+
+                startActivity(intent2);
+
+            }
+        };
+        btnMap2.setOnClickListener(oclbtnMap2);
+        View.OnClickListener oclbtnMap = new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent();
+
+                intent.setAction(Intent.ACTION_VIEW);
+
+                intent.setData(Uri.parse("http://www.vk.com"));
+
+                // Uri gmmIntentUri = Uri.parse("гео:37.7749,-122.4192?q=" + Uri.encode("1-й и Пайк, Сиэтл"));
+                // Uri gmmIntentUri = Uri.parse("гео:37.7749,-122.4194");
+
+                intent.setData(Uri.parse("geo:51.507351, -0.127696"));
+
+
+                startActivity(intent);
+
+            }
+        };
+        btnMap.setOnClickListener(oclbtnMap);
+        View.OnClickListener oclbtnMap3 = new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent3 = new Intent();
+
+                intent3.setAction(Intent.ACTION_VIEW);
+
+                intent3.setData(Uri.parse("http://www.vk.com"));
+
+                // Uri gmmIntentUri = Uri.parse("гео:37.7749,-122.4192?q=" + Uri.encode("1-й и Пайк, Сиэтл"));
+                // Uri gmmIntentUri = Uri.parse("гео:37.7749,-122.4194");
+
+                intent3.setData(Uri.parse("geo:50.080345, 14.428974"));
+
+
+                startActivity(intent3);
+
+            }
+        };
+        btnMap3.setOnClickListener(oclbtnMap3);
+
+        View.OnClickListener oclbtnMap4 = new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent4 = new Intent();
+
+                intent4.setAction(Intent.ACTION_VIEW);
+
+                intent4.setData(Uri.parse("http://www.vk.com"));
+
+                // Uri gmmIntentUri = Uri.parse("гео:37.7749,-122.4192?q=" + Uri.encode("1-й и Пайк, Сиэтл"));
+                // Uri gmmIntentUri = Uri.parse("гео:37.7749,-122.4194");
+
+                intent4.setData(Uri.parse("geo:50.080345, 14.428974"));
+
+
+                startActivity(intent4);
+
+            }
+        };
+        btnMap4.setOnClickListener(oclbtnMap4);
+
+        View.OnClickListener oclbtnMap5 = new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent5 = new Intent();
+
+                intent5.setAction(Intent.ACTION_VIEW);
+
+                intent5.setData(Uri.parse("http://www.vk.com"));
+
+                intent5.setData(Uri.parse("geo:52.518621, 13.375142"));
+
+                startActivity(intent5);
+
+            }
+        };
+        btnMap5.setOnClickListener(oclbtnMap5);
     }
 }

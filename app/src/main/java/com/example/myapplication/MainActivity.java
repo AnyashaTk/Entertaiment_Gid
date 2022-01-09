0ed1e71db96f75042775defc5fec4baaa2bef95e
NastyaGordienko <wind05@list.ru>
2022-01-09 16:57:30 +0300

    aaaaa

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,25 +1,116 @@
 package com.example.mapapp;
 
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.net.Uri;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RadioGroup radioGroup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       /* EditText textShirota = (EditText) findViewById(R.id.textShirota);
-        EditText textDolgota = (EditText) findViewById(R.id.textDolgota);
-       */
-        Button btnMap = (Button) findViewById(R.id.btnMap);
+        RadioGroup gender;
+
+        radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+        radioGroup.clearCheck();
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+
+                RadioButton rb = (RadioButton) group.findViewById(checkedId);
+
+            }
+        });
+    }
+
+
+    public void onClear(View v) {
+        radioGroup.clearCheck();
+    }
+
+    public void onSubmit(View v) {
+        Button button=(Button) findViewById(R.id.clearBtn);
+
+        RadioButton rb = (RadioButton) radioGroup.findViewById(radioGroup.getCheckedRadioButtonId());
+        int myRadioChecked;
+        if (radioGroup.getCheckedRadioButtonId() == findViewById(R.id.radioButton2).getId() && button.isEnabled()) {
+            Intent intent2 = new Intent();
+
+            intent2.setAction(Intent.ACTION_VIEW);
+
+            intent2.setData(Uri.parse("http://www.vk.com"));
+
+            intent2.setData(Uri.parse("geo:48.206487, 16.363460"));
+
+
+            startActivity(intent2);
+        } else if (radioGroup.getCheckedRadioButtonId() == findViewById(R.id.radioButton1).getId() && button.isEnabled()) {
+            Intent intent = new Intent();
+
+            intent.setAction(Intent.ACTION_VIEW);
+
+            intent.setData(Uri.parse("http://www.vk.com"));
+
+
+            intent.setData(Uri.parse("geo:50.080345, 14.428974"));
+
+
+            startActivity(intent);
+        }
+        else if (radioGroup.getCheckedRadioButtonId() == findViewById(R.id.radioButton1).getId() && button.isEnabled()) {
+            Intent intent3 = new Intent();
+
+            intent3.setAction(Intent.ACTION_VIEW);
+
+            intent3.setData(Uri.parse("http://www.vk.com"));
+
+
+            intent3.setData(Uri.parse("geo:52.518621, 13.375142"));
+
+
+            startActivity(intent3);
+        }
+        else if (radioGroup.getCheckedRadioButtonId() == findViewById(R.id.radioButton1).getId() && button.isEnabled()) {
+            Intent intent4 = new Intent();
+
+            intent4.setAction(Intent.ACTION_VIEW);
+
+            intent4.setData(Uri.parse("http://www.vk.com"));
+
+
+            intent4.setData(Uri.parse("geo:51.507351, -0.127696"));
+
+
+            startActivity(intent4);
+        }else {
+
+            Toast.makeText(MainActivity.this, "Ничего не выбрано!", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+       /* Button btnMap = (Button) findViewById(R.id.btnMap);
         Button btnMap2 = (Button) findViewById(R.id.btnMap2);
         Button btnMap3 = (Button) findViewById(R.id.btnMap3);
         Button btnMap4 = (Button) findViewById(R.id.btnMap4);
@@ -119,4 +210,7 @@ public class MainActivity extends AppCompatActivity {
         };
         btnMap5.setOnClickListener(oclbtnMap5);
     }
-}
+    }
+    */
+
+
